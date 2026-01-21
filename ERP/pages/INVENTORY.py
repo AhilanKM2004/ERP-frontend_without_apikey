b4acb927524ecffdf64cbfe71af5799d3f9fe262
K M Ahilan <ahilahilankm@gmail.com>
2026-01-21 10:44:41 +0530

    csv corrected version

--- ERP/pages/INVENTORY.py
+++ ERP/pages/INVENTORY.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from groq import Groq
 import pandas as pd
+from pathlib import Path
+
 
 if "login" not in st.session_state:
     st.session_state.login = False
@@ -15,51 +17,42 @@ elif (st.session_state.login == True and st.session_state.role == "admin"):
     if "readed" not in st.session_state:
         st.session_state.readed = None
     # file=st.file_uploader("please upload your project report - CSV",type = ["csv"])
-    file = "erp_dummy.csv"
-    if file:
-        with st.expander("RAW CSV file"):
-            readed = pd.read_csv(file)
-            st.session_state.readed = readed
-            st.table(readed)
-        if "readed" in st.session_state:
-            c1, c2 = st.columns(2)
 
-            with c1:
-                chart_options = ["line chart", "area chart", "scatter chart"]
-
-                if "radio" not in st.session_state:
-                    st.session_state.radio = chart_options[0]
-
-                st.session_state.radio = st.radio("SELECT DIAGRAM TYPE",chart_options)
-
-            with c2:
-        
-                df = st.session_state.readed
-
-                numeric_cols = df.select_dtypes(include="number").columns.tolist()
-
-                if st.session_state.radio == "line chart":
-                    st.line_chart(df[numeric_cols])
-
-                elif st.session_state.radio == "area chart":
-                    st.area_chart(df[numeric_cols])
-
-                elif st.session_state.radio == "scatter chart":
-                    st.scatter_chart(
-                        df,
-                        x=numeric_cols[0],
-                        y=numeric_cols[1]
-                    )
-
-            client = Groq(api_key="")
-            st.header("AI report")
-            st.write("i have removed api key - since someone told me to remove all api key before uploading your project into github")
-            system_prompt=f"""we will give you an csv file and you have to do analys over the file and make small report about the subjected file , declare the current state , postive , negative all important points , remember it's need not be a bigger report but important point , just for the clarification and finally once again make it very  shorter because longer version will be displayed after this portion so make don't make it over 25 lines, and the csv file is = {st.session_state.readed}"""
-            response = client.chat.completions.create(
-                    model="llama-3.3-70b-versatile",
-                    messages=[{"role": "system", "content": system_prompt}])
-            st.session_state.result = ( response.choices[0].message.content )
-            st.write(st.session_state.result)
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    file = pd.read_csv(BASE_DIR / "erp_dummy.csv")
+    st.session_state.readed = file
+    with st.expander("RAW CSV file"):
+        st.table(st.session_state.readed)
+    if "readed" in st.session_state:
+        c1, c2 = st.columns(2)
+        with c1:
+            chart_options = ["line chart", "area chart", "scatter chart"]
+            if "radio" not in st.session_state:
+                st.session_state.radio = chart_options[0]
+            st.session_state.radio = st.radio("SELECT DIAGRAM TYPE",chart_options)
+        with c2:
+    
+            df = st.session_state.readed
+            numeric_cols = df.select_dtypes(include="number").columns.tolist()
+            if st.session_state.radio == "line chart":
+                st.line_chart(df[numeric_cols])
+            elif st.session_state.radio == "area chart":
+                st.area_chart(df[numeric_cols])
+            elif st.session_state.radio == "scatter chart":
+                st.scatter_chart(
+                    df,
+                    x=numeric_cols[0],
+                    y=numeric_cols[1]
+                )
+        st.header("AI report")
+        st.write("i have removed api key - since someone told me to remove all api key before uploading your project into github")
+        client = Groq(api_key="")
+        system_prompt=f"""we will give you an csv file and you have to do analys over the file and make small report about the subjected file , declare the current state , postive , negative all important points , remember it's need not be a bigger report but important point , just for the clarification and finally once again make it very  shorter because longer version will be displayed after this portion so make don't make it over 25 lines, and the csv file is = {st.session_state.readed}"""
+        response = client.chat.completions.create(
+                model="llama-3.3-70b-versatile",
+                messages=[{"role": "system", "content": system_prompt}])
+        st.session_state.result = ( response.choices[0].message.content )
+        st.write(st.session_state.result)
 
 
     st.write("-----------------------------------------------------------------------------")
@@ -68,6 +61,7 @@ elif (st.session_state.login == True and st.session_state.role == "admin"):
         st.page_link(label = "To check sales details", page = "pages/SALES.py" )
     with c2:
         st.page_link(label = "To check product details", page = "pages/PRODUCTS.py" )
+
 
 elif (st.session_state.login == True and st.session_state.role == "common"):
     st.subheader("you are restricted")
