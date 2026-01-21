21288ef3a2c31411984e6b7418082e6d8a62c38a
K M Ahilan <ahilahilankm@gmail.com>
2026-01-21 15:23:36 +0530

    config 5

--- ERP/config.py
+++ ERP/config.py
@@ -8,9 +8,9 @@ load_dotenv()
 GROQ_API_KEY = (st.secrets.get("GROQ_API_KEY"))
 
 if not GROQ_API_KEY:
-    st.error("GROQ_API_KEY is missing")
     print("GROQ_API_KEY exists:", "GROQ_API_KEY" in os.environ)
     print("GROQ_API_KEY value :", os.getenv("GROQ_API_KEY"))
+    st.error("GROQ_API_KEY is missing")
     st.stop()
     
 print("GROQ_API_KEY exists:", "GROQ_API_KEY" in os.environ)
