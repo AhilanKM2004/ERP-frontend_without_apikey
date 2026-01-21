a16e878d5a6ee0dc1d833af20ba8a99c00dcb6ae
K M Ahilan <ahilahilankm@gmail.com>
2026-01-21 15:19:49 +0530

    config 4

--- ERP/config.py
+++ ERP/config.py
@@ -1,4 +1,4 @@
-
+import os
 import streamlit as st
 from dotenv import load_dotenv
 from groq import Groq
@@ -9,6 +9,10 @@ GROQ_API_KEY = (st.secrets.get("GROQ_API_KEY"))
 
 if not GROQ_API_KEY:
     st.error("GROQ_API_KEY is missing")
+    print("GROQ_API_KEY exists:", "GROQ_API_KEY" in os.environ)
+    print("GROQ_API_KEY value :", os.getenv("GROQ_API_KEY"))
     st.stop()
-
+    
+print("GROQ_API_KEY exists:", "GROQ_API_KEY" in os.environ)
+print("GROQ_API_KEY value :", os.getenv("GROQ_API_KEY"))
 client = Groq(api_key=GROQ_API_KEY)
